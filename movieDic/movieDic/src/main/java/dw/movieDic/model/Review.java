32e09142e3c60aa1415e5cded8b80a49a42c335d
Jae-hoDo <doje416@gmail.com>
2024-06-09 12:51:27 +0900

    Update Review.java

--- movieDic/movieDic/src/main/java/dw/movieDic/model/Review.java
+++ movieDic/movieDic/src/main/java/dw/movieDic/model/Review.java
@@ -1,4 +1,17 @@
 package dw.movieDic.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name = "review")
 public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long reviewId;
+    @ManyToOne
 }
