9122ecd50f11e3c3179fdce503652900f595d4dd
Jae-hoDo <doje416@gmail.com>
2024-06-09 19:54:31 +0900

    update

--- movieDic/movieDic/src/main/java/dw/movieDic/model/Review.java
+++ movieDic/movieDic/src/main/java/dw/movieDic/model/Review.java
@@ -3,6 +3,9 @@ package dw.movieDic.model;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDateTime;
+import java.util.Locale;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Setter
@@ -12,7 +15,23 @@ import lombok.*;
 public class Review {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "review_id")
     private long reviewId;
+
     @ManyToOne
-    @JoinColumn(name = )
+    @JoinColumn(name = "lecture_id", nullable = false)
+    private Lectures lecture;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "rating", nullable = false)
+    private int rating;
+
+    @Column(name = "review_text", nullable = false, length = 65535)
+    private String reviewText;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
 }
