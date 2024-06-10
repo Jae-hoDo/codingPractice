86ca2e2967ed32d1a52536cfb70a64bbbac50ea4
Jae-hoDo <doje416@gmail.com>
2024-06-10 23:53:50 +0900

    update

---
+++ movieDic_ByGum/src/main/java/dw/movieDic/Controller/ViewController.java
@@ -0,0 +1,40 @@
+package dw.movieDic.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+    @GetMapping("/login")
+    public String login_form(){
+        return "login_form";
+    }
+    @GetMapping("/articles")
+    public String articles(){
+        return "articles";
+    }
+    @GetMapping("/movieDic/index.html")
+    public String index(){
+        return "index";
+    }
+    @GetMapping("/movieDic/signup.html")
+    public String signup(){
+        return "signup";
+    }
+    @GetMapping("/movieDic/singleProduct.html")
+    public String singleProduct(){
+        return "singleProduct";
+    }
+    @GetMapping("/movieDic/login.html")
+    public String login(){
+        return "login";
+    }
+    @GetMapping("/movieDic/cart.html")
+    public String cart(){
+        return "cart";
+    }
+    @GetMapping("/movieDic/myPage.html")
+    public String myPage(){
+        return "myPage";
+    }
+}
