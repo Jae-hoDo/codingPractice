6fb1f6bf9bcaded07f940e6b32687238d08d64bb
Jae-hoDo <132924223+Jae-hoDo@users.noreply.github.com>
2024-06-10 10:33:06 +0900

    update

--- movieDic/movieDic/src/main/java/dw/movieDic/repository/PostRepository.java
+++ movieDic/movieDic/src/main/java/dw/movieDic/repository/PostRepository.java
@@ -3,5 +3,5 @@ package dw.movieDic.repository;
 import dw.movieDic.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends JpaRepository<Post,> {
+public interface PostRepository extends JpaRepository<Post,String> {
 }
--- movieDic/movieDic/src/main/java/dw/movieDic/service/PostService.java
+++ movieDic/movieDic/src/main/java/dw/movieDic/service/PostService.java
@@ -1,4 +1,11 @@
 package dw.movieDic.service;
 
+import dw.movieDic.repository.PostRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
 public class PostService {
+    private PostRepository postRepository;
 }
