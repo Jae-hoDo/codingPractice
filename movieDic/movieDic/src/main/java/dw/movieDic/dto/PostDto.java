2aafbe32bd3c1d28d572c7a075e8878bc3b4289d
Jae-hoDo <132924223+Jae-hoDo@users.noreply.github.com>
2024-06-10 11:34:33 +0900

    update

---
+++ movieDic/movieDic/src/main/java/dw/movieDic/dto/PostDto.java
@@ -0,0 +1,12 @@
+package dw.movieDic.dto;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class PostDto {
+    private String postTitle;
+    private String postContent;
+}
