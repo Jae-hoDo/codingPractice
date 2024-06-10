c5ef7e65e1732defdd5422d3a9fbd00542ea67d5
Jae-hoDo <132924223+Jae-hoDo@users.noreply.github.com>
2024-06-10 13:53:43 +0900

    update

--- movieDic/movieDic/src/main/java/dw/movieDic/repository/UserRepository.java
+++ movieDic/movieDic/src/main/java/dw/movieDic/repository/UserRepository.java
@@ -2,7 +2,6 @@ package dw.movieDic.repository;
 
 import dw.movieDic.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, String> {
--- movieDic/movieDic/src/main/java/dw/movieDic/service/UserService.java
+++ movieDic/movieDic/src/main/java/dw/movieDic/service/UserService.java
@@ -1,4 +1,21 @@
 package dw.movieDic.service;
 
+import dw.movieDic.dto.UserDto;
+import dw.movieDic.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.*;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserService {
+    private UserRepository userRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public String saveUser(UserDto userDto) {
+
+    }
 }
