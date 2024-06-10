461d9ee4c56446e98d322fb58bfed7330f409e98
Jae-hoDo <doje416@gmail.com>
2024-06-11 01:10:16 +0900

    update

--- movieDic_ByGun/src/main/java/dw/movieDic/Repository/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package dw.movieDic.Repository;
-
-import dw.movieDic.Model.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
--- movieDic_ByGun/src/main/java/dw/movieDic/Repository/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package dw.movieDic.Repository;
-
-import dw.movieDic.Model.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
