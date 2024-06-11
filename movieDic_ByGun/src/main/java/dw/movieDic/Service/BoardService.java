b9d369192815edd938fec9ee300d3a18051bdff9
Jae-hoDo <132924223+Jae-hoDo@users.noreply.github.com>
2024-06-11 10:26:30 +0900

    update

--- movieDic_ByGun/src/main/java/dw/movieDic/Service/BoardService.java
+++ movieDic_ByGun/src/main/java/dw/movieDic/Service/BoardService.java
@@ -1,4 +1,14 @@
 package dw.movieDic.Service;
 
+import dw.movieDic.Model.Board;
+import dw.movieDic.Repository.BoardRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
 public class BoardService {
+ /*   @Autowired
+    BoardRepository boardRepository;*/
 }
--- movieDic_ByGun/src/main/java/dw/movieDic/Service/PostService.java
+++ movieDic_ByGun/src/main/java/dw/movieDic/Service/PostService.java
@@ -1,4 +1,13 @@
 package dw.movieDic.Service;
 
+import dw.movieDic.Repository.PostRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
 public class PostService {
+//    private PostRepository postRepository;
+//
+
 }
