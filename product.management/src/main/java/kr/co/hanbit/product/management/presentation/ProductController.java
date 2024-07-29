d4d21f9a4aa36bbcd31b026640b4b3220d24b5ea
Jae-hoDo <doje416@gmail.com>
2024-07-30 02:32:22 +0900

    Update ProductController.java

--- product.management/src/main/java/kr/co/hanbit/product/management/presentation/ProductController.java
+++ product.management/src/main/java/kr/co/hanbit/product/management/presentation/ProductController.java
@@ -1,6 +1,8 @@
 package kr.co.hanbit.product.management.presentation;
 
+import kr.co.hanbit.product.management.application.SimpleProductService;
 import kr.co.hanbit.product.management.domain.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,6 +11,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ProductController {
 
+    private SimpleProductService simpleProductService;
+
+    @Autowired
+    ProductController(SimpleProductService simpleProductService) {
+        this.simpleProductService = simpleProductService;
+    }
+    
     @RequestMapping(value = "/products", method = RequestMethod.POST)
     public Product createProduct(@RequestBody Product product) {
         // Product를 생성하고 리스트에 넣는 작업이 필요함.
