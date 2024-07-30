ce104adae3b80855739f6af63c4bea7cba6d06e4
Jae-hoDo <doje416@gmail.com>
2024-07-31 01:08:09 +0900

    update

--- bookManagementSystem/src/main/java/dw/bookManagementSystem/Service/BookService.java
+++ bookManagementSystem/src/main/java/dw/bookManagementSystem/Service/BookService.java
@@ -15,7 +15,10 @@ public class BookService {
         this.bookRepository = bookRepository;
     }
 
+    // 데이터베이스의 도서 정보 조회
     public List<Book> getAllBooks() {
         return bookRepository.findAll();
     }
+    
+    
 }
