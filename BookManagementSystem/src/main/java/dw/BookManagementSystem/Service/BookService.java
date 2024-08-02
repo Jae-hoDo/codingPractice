44810d1d162eb17996153495ae49226c3423f841
Jae-hoDo <132924223+Jae-hoDo@users.noreply.github.com>
2024-08-02 15:09:43 +0900

    Update BookService.java

--- BookManagementSystem/src/main/java/dw/BookManagementSystem/Service/BookService.java
+++ BookManagementSystem/src/main/java/dw/BookManagementSystem/Service/BookService.java
@@ -5,6 +5,7 @@ import dw.BookManagementSystem.Repository.BookRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BookService {
@@ -18,5 +19,25 @@ public class BookService {
     // 데이터베이스의 도서 정보 조회
     public List<Book> getAllBooks() {
         return bookRepository.findAll();
+    }
+
+    public Book updateBook(String oldIsbn, Book updatedBook) {
+        Optional<Book> existingBook = bookRepository.findById(oldIsbn);
+        if (existingBook.isPresent()) {
+            if (!oldIsbn.equals(updatedBook.getIsbn())) {
+                bookRepository.deleteById(oldIsbn);
+                return bookRepository.save(updatedBook);
+            } else {
+                Book bookToUpdate = existingBook.get();
+                bookToUpdate.setIsbn(updatedBook.getIsbn());
+                bookToUpdate.setTitle(updatedBook.getTitle());
+                bookToUpdate.setAuthor(updatedBook.getAuthor());
+                bookToUpdate.setYearPublication(updatedBook.getYearPublication());
+                bookToUpdate.setStock(updatedBook.getStock());
+
+                return bookRepository.save(bookToUpdate);
+            }
+        }
+        return null;
     }
 }
