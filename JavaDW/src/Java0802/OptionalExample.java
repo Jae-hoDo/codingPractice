35bfc51608c31f66adc078c42230d8457ed4e52d
Jae-hoDo <132924223+Jae-hoDo@users.noreply.github.com>
2024-08-02 17:22:30 +0900

    Update OptionalExample.java

--- JavaDW/src/Java0802/OptionalExample.java
+++ JavaDW/src/Java0802/OptionalExample.java
@@ -2,16 +2,7 @@ package Java0802;
 
 public class OptionalExample {
     public static void main(String[] args) {
-        // Optional 사용하지 않은 경우
-        String name = getName();
-        if (name  != null) {
-            System.out.println(name.length());    // NPE 가능성 있음
-        }
-    }
-
-    // null을 반환할 수 있는 메서드
-    public static String getName() {
-        // 실제로는 null을 반환할 수 있는 로직
-        return null;
+        String str = null;
+        System.out.println(str.length());
     }
 }
